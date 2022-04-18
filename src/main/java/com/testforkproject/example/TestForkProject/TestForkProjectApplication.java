f4ff424884052121b5f9c627da93f24e66b29a45
TariQwerty <103945172+TariQwerty@users.noreply.github.com>
2022-04-18 21:02:41 +0530

    Testing fork repository

--- src/main/java/com/testforkproject/example/TestForkProject/TestForkProjectApplication.java
+++ src/main/java/com/testforkproject/example/TestForkProject/TestForkProjectApplication.java
@@ -9,5 +9,11 @@ public class TestForkProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestForkProjectApplication.class, args);
 	}
+	
+	public void testFork(){
+	
+		 System.out.println("Testing fork repository:");
+	
+	}
 
 }
